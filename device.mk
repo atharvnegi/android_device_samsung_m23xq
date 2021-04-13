bea82540d876eb8b9580f9d7ef9059dfd61e6f64
AdarshGrewal <adarshgrewal@gmail.com>
2021-04-13 13:50:17 +0200

    m23xq: build telephony packages
    Change-Id: I2fa801e3e9985bad1fa2f189982ca39d46715eee

--- device.mk
+++ device.mk
@@ -84,5 +84,22 @@ PRODUCT_SHIPPING_API_LEVEL := 31
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.telephony.ims.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/android.hardware.telephony.ims.xml
+
+# Telephony
+PRODUCT_PACKAGES += \
+    ims-ext-common \
+    ims_ext_common.xml \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext \
+
+PRODUCT_BOOT_JARS += \
+    telephony-ext
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/samsung/m23xq/m23xq-vendor.mk)
