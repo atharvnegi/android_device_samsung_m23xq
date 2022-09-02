20daed9628df029fbb0211e2e7876a593f3edfab
Alone0316 <rutujamandlik06@gmail.com>
2022-09-02 14:34:49 +0000

    m23xq: Switch to permissive selinux for now [TMP]

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -55,6 +55,7 @@ TARGET_RECOVERY_DEVICE_MODULES := libinit_a52sxq
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=qcom androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 cgroup.memory=nokmem,nosocket firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_RAMDISK_OFFSET := 0x02000000
 BOARD_KERNEL_TAGS_OFFSET := 0x01e00000
